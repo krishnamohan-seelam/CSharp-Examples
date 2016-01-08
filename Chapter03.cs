a3334cf77f202e6611ff80a020d6dc9e8419d98b
krishnamohan-seelam <krishnamohan.seelam@outlook.com>
2016-01-08 08:00:26 +0530

    Reformatted few things

--- Chapter03.cs
+++ Chapter03.cs
@@ -14,7 +14,7 @@ using System.Reflection;
  *  as          --- as operator performs only reference conversions, nullable conversions and boxing conversions.
  *                  The as operator can't perform other conversions, such as user-defined conversions (int to double )
  */
-namespace SSIS_Scripting
+namespace LearningCS
 {
     class Chapter03
     {
@@ -36,7 +36,7 @@ namespace SSIS_Scripting
             int? a = null;// when your using nullable types always check HasValue before using it in computations
 
             int b = a ?? -1;
-            
+            Console.WriteLine("Examples on Nullable types and Nullable operator ... ");
             Console.WriteLine("value of b {0}" ,b);
             int c = GetNullInt() ?? default(int);
             Console.WriteLine("value of c {0}",c);
@@ -50,8 +50,86 @@ namespace SSIS_Scripting
             int sumValue = 10 +  (a.HasValue ? a.Value : default(int));
             Console.WriteLine("Sum of value ={0}", sumValue);
 
+            Console.WriteLine("Example base and derived:");
 
+            Parent baseRef = new Parent();
+            baseRef.printFunc();
+
+            Child derRef = new Child();
+            derRef.printFunc();
+
+            Console.WriteLine("Example base and derived using as : ");
+
+            Parent newRef = derRef as Parent;
+            Console.WriteLine("Type of newRef is {0}", newRef.GetType());
+            newRef.printFunc();
+            
+
+            Console.WriteLine("Press any key to exit ....");
             Console.ReadKey();
         }
+
+
     }
+
+    class Parent
+    {
+        public Parent()
+        {
+        }
+
+        public void Inherit()
+        {
+            Console.WriteLine("from  Parent Inherit()");
+        }
+        public virtual void printFunc()
+        {
+            Console.WriteLine("Called From {0} , {1} method", MethodBase.GetCurrentMethod().DeclaringType ,MethodBase.GetCurrentMethod().Name);
+        }
+
+    }
+
+    class Child : Parent
+    {
+
+        public Child()
+        {
+        }
+
+        /* 
+        * only methods declared as virtual/abstract/override can be overriden
+          public override  void Inherit()
+            {
+            Console.WriteLine("from  Parent Inherit()");
+             }
+        
+         */
+        public override sealed void printFunc()
+        {
+            
+            Console.WriteLine("Called From {0} , {1} method", MethodBase.GetCurrentMethod().DeclaringType ,MethodBase.GetCurrentMethod().Name);
+        }
+
+       
+    }
+
+
+    class GrandChild : Child
+    {
+        public GrandChild()
+        {
+
+        }
+
+        /*
+         * sealed methods cannot be overriden
+         public override void printFunc()
+        {
+
+        }*/
+
+
+    }
+
+
 }
